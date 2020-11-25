d46a2d6b73354cf49dfe03d4423690c2f13863a3
Michael Rodriguez <73129506+maicolrg11@users.noreply.github.com>
2020-11-25 18:26:38 -0500

    vista insumo

--- sscultivo/templates/Insumos/add.php
+++ sscultivo/templates/Insumos/add.php
@@ -16,33 +16,37 @@ endforeach;
         <p class="h1 mb-2">Agregar nuevo insumo</p>
         <?php
         echo $this->Form->control('nombre', ['class' => 'form-control']);
-        echo $this->Form->control('precio_unitario', ['class' => 'form-control', 'min' => '1', 'onchange' => 'precio()']);
+        echo $this->Form->control('precio_unitario', ['id' => 'precio_unitario', 'name' => 'precio_unitario', 'class' => 'form-control', 'min' => '1', 'onchange' => 'precio()']);
         ?>
-        <label for="nombre">Cantidad</label>
+        <label>Cantidad</label>
         <input type="number" name="cantidad" id="cantidad" class="form-control" min='1' onchange="precio()">
+        <input type="number" id="precio_total" name="precio_total">
         <?php
-        echo $this->Form->control('precio_total', ['id' => 'precio_total', 'name' => 'precio_total', 'class' => 'form-control', 'disabled']);
+        // echo $this->Form->control('precio_total', ['id' => 'precio_total', 'name' => 'precio_total', 'class' => 'form-control',]);
         echo $this->Form->control('cultivos_id', ['options' => $opcion, 'class' => 'form-control mb-4']);
         ?>
 
-        <?= $this->Form->button('Guardar monitoreo', ['class' => 'btn btn-success']) ?>
+        <?= $this->Form->button('Guardar monitoreo', ['class' => 'btn btn-success', 'onchange' => 'precio()']) ?>
         <?= $this->Form->end() ?>
     </center>
 </div>
 
 <script>
-    let valor = document.getElementsById("precio_total");
-    valor.value = 12314;
+    
+    alert("hola");
+    document.getElementsById("precio_unitario").value = '12';
+    document.getElementsById("cantidad").value = '12';
+    let h = document.getElementsById("precio_total");
+    h.value = '1200';
 
     function precio() {
         let precio = document.getElementsById("precio_unitario").value;
         let cantidad = document.getElementsById("cantidad").value;
-        let total = precio * cantidad;
+        let total = 0;
+        total = precio * cantidad;
         alert(precio);
         alert(cantidad);
-        if ((precio != 0) && (cantidad != 0)) {
-            let valor = document.getElementsById("precio_total");
-            valor.value = total;
-        }
+        let valor = document.getElementsById("precio_total");
+        valor.value = total;
     }
 </script>
