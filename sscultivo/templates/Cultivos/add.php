31b48669914c429195dee28877ab5029e92cddea
Michael Rodriguez <73129506+maicolrg11@users.noreply.github.com>
2020-11-26 11:25:48 -0500

    cultivo buton

--- sscultivo/templates/Cultivos/add.php
+++ sscultivo/templates/Cultivos/add.php
@@ -12,6 +12,14 @@ $opcion = [
     'pez gato' => 'Pez gato',
     'truca arcoiris' => 'Truca arcoiris'
 ];
+
+$opcion2 = [
+    'Seleccionar planta',
+    'pepino' => 'Pepino',
+    'tomate' => 'Tomate',
+    'brocoli' => 'Pez gato',
+    // 'truca arcoiris' => 'Truca arcoiris'
+];
 ?>
 <div class="container">
     <?= $this->Form->create($cultivo, ['class' => 'text-center border border-light p-4']) ?>
@@ -33,8 +41,8 @@ $opcion = [
                 ])
                 ?>
                 <div id="peces" style="display:none;" class="mt-4">
-                    <select name="pez" id="pez" placeholder="Tipo de cultivo" class="form-control" onchange="desplegar();">
-                        <option value="0">Seleccionar tipo pez</option>
+                    <select name="pez" id="pez" placeholder="Tipo de cultivo" class="form-control"">
+                        <option value=" 0">Seleccionar tipo pez</option>
                         <option value="carpa comun">Carpa comun</option>
                         <option value="tilapia del nilo">Tilapia del nilo</option>
                         <option value="pez gato">Pez gato</option>
@@ -72,7 +80,7 @@ $opcion = [
                 <?php echo $this->Form->control('usuario_id', ['type' => 'hidden', 'value' => $_SESSION['id']]); ?>
             </fieldset>
             <br>
-            <?= $this->Form->button('Guardar cultivo', ['class' => 'btn btn-success']) ?>
+            <?= $this->Form->button('Guardar cultivo', ['id' => 'btn','class' => 'btn btn-success', 'style', 'display:none;']) ?>
             <?= $this->Form->end() ?>
         </div>
     </center>
@@ -87,6 +95,7 @@ $opcion = [
             document.getElementById("plantas").style.display = "block";
             document.getElementById("plantas").style = "form-row";
             document.getElementById("peces").style.display = "none";
+            document.getElementById("btn").style.display = "block";
         } else if (tipo == 'Acuaponico') {
             document.getElementById("datos").style.display = "block";
             document.getElementById("datos").style = "form-row";
@@ -94,10 +103,12 @@ $opcion = [
             document.getElementById("plantas").style = "form-row";
             document.getElementById("peces").style.display = "block";
             document.getElementById("peces").style = "form-row";
+            document.getElementById("btn").style.display = "block";
         } else {
             document.getElementById("datos").style.display = "none";
             document.getElementById("plantas").style.display = "none";
             document.getElementById("peces").style.display = "none";
+            document.getElementById("btn").style.display = "none";
         }
     }
 </script>
